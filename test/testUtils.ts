29a23e7c27bc191f195db8bb7e55adafeda186f0
Nick Rook <rooknj@outlook.com>
2020-06-09 16:56:41 -0400

    chore: updated package versions

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const Providers = ({ children }) => {
   return children;
@@ -15,10 +15,10 @@ const Providers = ({ children }) => {
   //   </ThemeProvider>
   // )
 };
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const customRender = (ui, options = {}) =>
-  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   render(ui, { wrapper: Providers, ...options });
 
