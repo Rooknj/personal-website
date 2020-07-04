1f0b1bd2088481924cd0bb152c2d4f42c79bdf8c
Nick Rook <rooknj@outlook.com>
2020-07-04 14:41:48 -0400

    chore: update eslintrc rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,14 @@ module.exports = {
   extends: ["@rooknj/eslint-config"],
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": 0,
+    // This rule is a workaround for Next.js Link API behavior https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
   },
 };
