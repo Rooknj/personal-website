2888b19d4afb577ef880d4ea915a12254e87ad7a
Nick Rook <rooknj@outlook.com>
2020-09-26 23:12:42 -0400

    chore: updated dev deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
-// const extraRules = {
-//   // This rule is a workaround for Next.js Link API behavior https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
-//   "jsx-a11y/anchor-is-valid": [
-//     "error",
-//     {
-//       components: ["Link"],
-//       specialLink: ["hrefLeft", "hrefRight"],
-//       aspects: ["invalidHref", "preferButton"],
-//     },
-//   ],
-// };
-
-module.exports = {};
+module.exports = {
+  extends: ["@rooknj/eslint-config"],
+  rules: {
+    // This rule is a workaround for Next.js Link API behavior https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+  },
+};
