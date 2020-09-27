b748b42c7f2f2541a16ee7e4b43b5abfef738d6a
Nick Rook <rooknj@outlook.com>
2020-09-26 21:51:46 -0400

    chore: got eslint working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
-module.exports = {
-  extends: ["@rooknj/eslint-config"],
-  rules: {
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    // This rule is a workaround for Next.js Link API behavior https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
-    "jsx-a11y/anchor-is-valid": [
-      "error",
-      {
-        components: ["Link"],
-        specialLink: ["hrefLeft", "hrefRight"],
-        aspects: ["invalidHref", "preferButton"],
-      },
-    ],
-  },
-};
+// const extraRules = {
+//   // This rule is a workaround for Next.js Link API behavior https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
+//   "jsx-a11y/anchor-is-valid": [
+//     "error",
+//     {
+//       components: ["Link"],
+//       specialLink: ["hrefLeft", "hrefRight"],
+//       aspects: ["invalidHref", "preferButton"],
+//     },
+//   ],
+// };
+
+module.exports = {};
